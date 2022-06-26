ad3769f08843be3e19ad11dea8745a97f5eb2962
danilivanyuk <danilivanyuk00@gmail.com>
2022-06-26 15:19:40 +0600

    add router

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Homepage from "./homepage/Homepage";
+import Homepage from "./Homepage/Homepage";
 import Navbar from "./Navbar/Navbar";
 
 import "./static/main.css";
@@ -9,7 +9,20 @@ import "./static/main.css";
 export default function App() {
   return (
     <div className="container mx-auto">
-      <Navbar />
+      <Router basename="/">
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route path="/">
+              <Homepage />
+            </Route>
+            {/* <Route path="/profile">
+                <Profile/>
+              </Route> */}
+          </Switch>
+        </Fragment>
+      </Router>
+
       <Homepage />
     </div>
   );
